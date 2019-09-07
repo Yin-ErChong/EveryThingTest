968d7465859aeff6c039c448eda227e74f7bbf11
尔冲 尹 <936162557@qq.com>
2019-09-07 14:39:19 +0800

    添加项目文件。

---
+++ BaseClass/TestBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveryThingTest.BaseClass
+{
+    public interface ITestBase
+    {
+         void Start();
+         void End();
+    }
+    public abstract class TestBase
+    {
+        public abstract void Start();
+        public virtual void End() {
+            Console.WriteLine("It'End");
+            Console.ReadLine();
+
+        }
+
+        public void Begin()
+        {
+            Start();
+            End();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using EveryThingTest.InstanceClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveryThingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstTest.Instance.Begin();
+        }
+    }
+}
