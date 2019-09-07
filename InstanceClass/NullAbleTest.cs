dcfc6caa80a5104fe40ca7a09fc06c5ec67f9f0f
尔冲 尹 <936162557@qq.com>
2019-09-07 15:06:41 +0800

    新增不可为空Test

---
+++ InstanceClass/NullAbleTest.cs
@@ -0,0 +1,34 @@
+﻿using EveryThingTest.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveryThingTest.InstanceClass
+{
+
+    public class NullAbleTest : TestBase, ITestBase
+    {
+        private static NullAbleTest _Instance;
+        public static NullAbleTest Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new NullAbleTest();
+                }
+                return _Instance;
+            }
+            set
+            {
+                _Instance = value;
+            }
+        }
+        public override void Start()
+        {
+            Console.WriteLine("It'Start");
+        }
+    }
+}
