f169be11e4ab00f1cc44a4a81bf8f423199e5723
尔冲 尹 <936162557@qq.com>
2019-09-17 00:47:23 +0800

    客户端增加连接方法

--- Helper/TcpSocketClient.cs
+++ Helper/TcpSocketClient.cs
@@ -1,16 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EveryThingTest.Helper
 {
+   
     class TcpSocketClient
     {
-        public void StartConnect()
+        Socket socketClient = null;
+        public bool OpenClient()
         {
+            socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // 创建包含ip和端口号的网络节点对象；
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 9361);
+            try
+            {
+                socketClient.Bind(endPoint);
+            }
+            catch (Exception)
+            {
 
+                return false;
+            }
+            return true;
+        }
+        public bool StartConnect(IPEndPoint endPoint)
+        {
+            socketClient.Connect(endPoint);//https://www.cnblogs.com/longdb/articles/7015827.html
         }
     }
 }
