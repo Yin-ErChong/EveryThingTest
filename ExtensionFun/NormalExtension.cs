db006f4a3e9ee60f0ec42534674024af2092b792
Yin <yinerchong@beisen.com>
2019-09-18 11:39:05 +0800

    增加List分割方法

--- ExtensionFun/NormalExtension.cs
+++ ExtensionFun/NormalExtension.cs
@@ -25,6 +25,26 @@ namespace EveryThingTest.ExtensionFun
             }
             return nullObject.Value;
         }
+        /// <summary>
+        /// 分割List
+        /// </summary>
+        /// <typeparam name="T">源数据类型</typeparam>
+        /// <param name="source">源数据</param>
+        /// <param name="capacity">容量（分割单位）</param>
+        /// <returns></returns>
+        public static List<List<T>> SplitList<T>(List<T> source, int capacity) where T : class
+        {
+            List<List<T>> target = new List<List<T>>();
+            for (int i = 0; i <= (source.Count() - 1) / capacity; i++)
+            {
+                target.Add(new List<T>());
+            }
+            for (int i = 0; i < source.Count; i++)
+            {
+                target[i / capacity].Add(source[i]);
+            }
+            return target;
+        }
 
     }
 }
