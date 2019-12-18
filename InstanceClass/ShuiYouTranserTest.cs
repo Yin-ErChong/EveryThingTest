1d4d5fde505e3c9cb2b1ddf87ed013e0d835e85b
yinerchong <yinerchong@beisen.com>
2019-12-18 16:04:57 +0800

    增加类

---
+++ InstanceClass/ShuiYouTranserTest.cs
@@ -0,0 +1,37 @@
+﻿using EveryThingTest.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveryThingTest.InstanceClass
+{
+    public class ShuiYouTranserTest : TestBase, ITestBase
+    {
+        private static ShuiYouTranserTest _Instance;
+        public static ShuiYouTranserTest Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new ShuiYouTranserTest();
+                }
+                return _Instance;
+            }
+            set
+            {
+                _Instance = value;
+            }
+        }
+        public override void Start()
+        {
+            Console.WriteLine("It'Start");
+        }
+        public override void End()
+        {
+            Console.WriteLine("It'End");
+        }
+    }
+}
