29d57bdca7a15dae9b334f1f26db5926a66291d3
尔冲 尹 <936162557@qq.com>
2020-06-18 21:27:01 +0800

    增加http测试

---
+++ InstanceClass/HttpRequestTest.cs
@@ -0,0 +1,33 @@
+﻿using EveryThingTest.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveryThingTest.InstanceClass
+{
+    public class HttpRequestTest : TestBase, ITestBase
+    {
+        private static HttpRequestTest _Instance;
+        public static HttpRequestTest Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new HttpRequestTest();
+                }
+                return _Instance;
+            }
+            set
+            {
+                _Instance = value;
+            }
+        }
+        public override void Start()
+        {
+            Console.WriteLine("It'Start");
+        }
+    }
+}
