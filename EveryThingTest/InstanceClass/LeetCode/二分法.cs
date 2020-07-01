1400f6394bd2192f130cc951bdb0b6aa11f7bb31
yinec <yinec>
2020-07-01 13:23:47 +0800

    增加二分法

---
+++ EveryThingTest/InstanceClass/LeetCode/二分法.cs
@@ -0,0 +1,51 @@
+﻿using EveryThingTest.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveryThingTest.InstanceClass.LeetCode
+{
+    public class 二分法 : TestBase, ITestBase
+    {
+        private static 二分法 _Instance;
+        public static 二分法 Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new 二分法();
+                }
+                return _Instance;
+            }
+            set
+            {
+                _Instance = value;
+            }
+        }
+        public override void Start()
+        {
+            Console.WriteLine("It'Start");
+        }
+        public int GetResult(int[] intArray,int number)
+        {
+            int seq = -1;
+            int mid = intArray.Length/2;
+            int left = 0;
+            int right = intArray.Length - 1;
+            while (true)
+            {
+                if (left==right)
+                {
+                    return seq;
+                }
+                if (number> intArray[left])
+                {
+
+                }
+            }
+        }
+    }
+}
