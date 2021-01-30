47571b2b246ec1f9037b50820a2a6dd94dee9ef5
尔冲 尹 <936162557@qq.com>
2021-01-30 13:12:05 +0800

    增加

---
+++ Model/BaseModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace EveryThingTest.Model
+{
+    public  class BaseModel
+    {
+        #region 公共字段
+        public int Id { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime ModifiedTime { get; set; }
+        public int Enable { get; set; }
+        #endregion
+        private static Dictionary<string, Dictionary<string, PropertyInfo>> propertyInfoDic = new Dictionary<string, Dictionary<string, PropertyInfo>>();
+        private static object locker = new object();
+        private Type type ;
+        public BaseModel()
+        {
+            type = this.GetType();
+        }
+        public void SetDefault()
+        {
+            InitPropertyInfo(type);
+            foreach (var item in propertyInfoDic[type.ToString()])
+            {
+                Type itemType = item.Value.PropertyType;
+                switch (itemType.Name)
+                {
+                    case "String": SetValue(item.Key, string.Empty); break;                   
+                }
+                if (item.Key=="")
+                {
+
+                }
+            }
+        }
+        public static void InitPropertyInfo(Type type)
+        {
+            string typeName = type.ToString();
+            if (!propertyInfoDic.ContainsKey(typeName))
+            {
+                lock (locker)
+                {
+                    propertyInfoDic.Add(typeName, new Dictionary<string, PropertyInfo>());
+                    foreach (var item in type.GetProperties())
+                    {
+                        propertyInfoDic[typeName].Add(item.Name, item);
+                    }
+                }
+            }
+        }
+        public void SetValue(string properName, object value)
+        {
+            InitPropertyInfo(type);
+            string typeName = type.ToString();
+            if (propertyInfoDic[typeName].ContainsKey(properName))
+            {
+                //propertyInfoDic[typeName][properName].SetValue(this, value);
+            }
+        }
+    }
+}
---
+++ Model/序列化Test.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EveryThingTest.Model
+{
+	public class UserInfo : BaseModel
+	{
+		public string Url { get; set; }
+		public string UserName { get; set; }
+		public string PassWord { get; set; }
+		public string IsLog { get; set; }
+		public string Cookie { get; set; }
+	}
+	public class 序列化ModelTest:BaseModel
+    {
+		/// <summary>
+		/// 
+		/// </summary>
+		public DateTime TransactionTime { set; get; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public string TransactionType { set; get; }
+		/// <summary>
+		/// 支出金额
+		/// </summary>
+		public decimal TradeOutAmount { set; get; }
+		/// <summary>
+		/// 收入金额
+		/// </summary>
+		public decimal TradeInAmount { set; get; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public string MoneyType { set; get; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public decimal? Balance { set; get; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public string OtherAccountName { set; get; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public string OtherAccountNumber { set; get; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Summary { set; get; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public int TurnoverFileId { set; get; }
+	}
+}
